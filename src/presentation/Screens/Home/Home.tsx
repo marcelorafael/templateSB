33869511e99e3e157dbcab99e2e6c5307f7a2654
marcelorafael <marcelorafael23@hotmail.com>
2021-07-22 19:21:15 -0300

    feat: Storybook working

---
+++ src/presentation/Screens/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Title } from './Home.styles';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <Title>Home</Title>
+    </Container>
+  )
+}
+const HomeMemo = React.memo(Home);
+export { HomeMemo as Home }
---
+++ storybook/storyLoader.js
@@ -0,0 +1,19 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+  require("../src/presentation/Screens/Details/Details.stories");
+  require("../src/presentation/Screens/Home/Home.stories");
+}
+
+const stories = [
+  "../src/presentation/Screens/Details/Details.stories",
+  "../src/presentation/Screens/Home/Home.stories",
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
